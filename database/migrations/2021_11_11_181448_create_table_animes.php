4f855f3d9ed221ecffe10e404168f4470572d9e9
Ana Gabriela <anagabrieladev@gmail.com>
2021-12-02 00:10:04 -0300

    🐛 bugfix: correct name of function

--- database/migrations/2021_11_11_181448_create_table_animes.php
+++ database/migrations/2021_11_11_181448_create_table_animes.php
@@ -26,6 +26,6 @@ class CreateTableAnimes extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_animes');
+        Schema::dropIfExists('animes');
     }
 }
